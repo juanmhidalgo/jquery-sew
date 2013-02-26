52bf0638ebcfb9f51266ab4ca7090f6e86ba2d3d
Juan M. Hidalgo <juanmhidalgo@gmail.com>
2013-02-26 14:52:43 -0300

    Fix "this" bug on getMentions

--- jquery.sew.js
+++ jquery.sew.js
@@ -1,3 +1,20 @@
+/**
+* The MIT License (MIT)
+* Copyright (c) 2012 @leChanteaux (santiago at mural.ly)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+* associated documentation files (the "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+* LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 //https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/filter
 if (!Array.prototype.filter){
 	Array.prototype.filter = function(fun /*, thisp */){
@@ -536,12 +553,12 @@ if (!Array.prototype.map) {
 			target = [],
 			txt = this.getText(),
 			men = this.mentions.filter(function(el){
-				var exp = new RegExp('\\W*' + that.options.token + el[this.options.val] + '(\\W|$)');
+				var exp = new RegExp('\\W*' + that.options.token + el[that.options.val] + '(\\W|$)');
 				return txt.match(exp);
 			});
 
 			men.forEach(function (e) {
-				var hasElement = target.map(function (j) { return j[this.options.val]; }).indexOf(e[this.options.val]) >= 0;
+				var hasElement = target.map(function (j) { return j[that.options.val]; }).indexOf(e[that.options.val]) >= 0;
 				if(hasElement){
 					return;
 				}
